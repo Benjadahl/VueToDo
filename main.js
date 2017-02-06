7dd18d1de53bdff6002084f2a10b5d2d047c3d2c
Benjadahl <benjadahl@gmail.com>
2017-02-06 22:25:43 +0100

    Added categories

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+Vue.component("jsonexplorer", {
+  props:['object'],
+
+  data: function () {
+    return {
+      selected: Object.keys(this.object)[0]
+    };
+  },
+
+  template:"<select v-model='selected'><option v-for='(value, key) in object'>{{ key }}</option></select>",
+
+  created: function () {
+    this.$emit("new-item-selected", this.selected);
+  },
+
+  watch: {
+    selected: function () {
+      this.$emit("new-item-selected", this.selected);
+    }
+  }
+});
+
 Vue.component("todotask" ,{
   props:["task"],
   data: function () {
@@ -5,7 +27,7 @@ Vue.component("todotask" ,{
       shown: true
     }
   },
-  template:"<li v-if='shown'><input v-on:change='test' type='checkbox'/>{{ task }}</li>",
+  template:"<div v-if='shown'><input v-on:change='test' type='checkbox'/>{{ task }}<br /></div>",
   methods: {
     test: function () {
       this.shown = false;
@@ -17,16 +39,24 @@ var app = new Vue({
   el: "#vueApp",
   data: {
     input: "Enter description for a task",
-    tasks: [
-      "Do the dishes",
-      "Create a nice Vue app",
-      "Sleep"
-    ]
+    category: "urgent",
+    tasks: {
+      urgent:[
+        "Create a nice Vue app",
+        "Sleep"
+      ],
+      nonImportant:[
+        "Do the dishes"
+      ]
+    }
   },
   methods: {
     addNewTask: function () {
-      this.tasks.push(this.input);
+      this.tasks[this.category].push(this.input);
       this.input = "";
+    },
+    updateCategory: function (test) {
+      this.category = test;
     }
   }
 });
